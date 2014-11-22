a9cde14147a6c11cb72397adb290fe8d75a794b8
DuckBossa <DuckBossa@gmail.com>
2014-11-22 21:19:40 +0800

    collision still updating

--- hw02.java
+++ hw02.java
@@ -52,10 +52,9 @@ public class hw02{
 		frame.add(canvas);
 	}
 
-
     public void movementBall(){ 
         // collision detection for the ball | we can also check in this method kung may nanalo na ba o wala.
-        if( (cx - rad) < (p1x + rw - 5) && ((cy+rad) <= (p1y + rh) || (cy - rad) >= p1y) ){ 
+        if( (cx - rad) < (p1x + rw - 5) && ((cy+rad) <= (p1y + rh) || (cy - rad) >= p1y) ){ // this method not working! D:
             System.out.println("Circle X: " + (cx - rad));
             System.out.println("Rectangle X: " + (p1x + rw));
             System.out.println("Rectangle Y: " + (p1y + rh) );
@@ -113,7 +112,6 @@ public class hw02{
             }
         }
     }
-
 
     class kl implements KeyListener{
         @Override
